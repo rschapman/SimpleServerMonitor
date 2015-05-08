43c7c7ffb8ab69dd6e424c10cd2bac204c8fe0cf
Rob Chapman <rschapman@gmail.com>
2015-05-08 16:06:15 -0500

    Init Commit
    Initial Commit of Files

---
+++ Ping_Multiple_Servers.ps1
@@ -0,0 +1,21 @@
+﻿#Author: Robert Chapman    
+#Email: rschapman@gmail.com
+#Purpose: Ping Multiple Servers / Computers       
+#Version: 1
+#"d-bwqa", "d-bwjs",
+
+$ServerName = "d-bwjd","d-bwjq","d-bwjp","d-bobjs","d-bobjd","d-bobjqa","d-bobjp","d-bws","d-bwd","d-bwq","d-bwp"
+
+foreach ($Server in $ServerName) {
+
+		if (test-Connection -ComputerName $Server -Count 2 -Quiet ) { 
+		
+			write-Host "$Server is alive." -ForegroundColor Green
+		
+					} else
+					
+						{ Write-Warning "$Server may not be alive."
+			
+					}	
+		
+}
---
+++ Ping_Multiple_Servers_Loop.ps1
@@ -0,0 +1,24 @@
+﻿#Author: Robert Chapman    
+#Email: rschapman@gmail.com
+#Purpose: Ping Multiple Servers / Computers       
+#Version: 1
+#"d-bwqa", "d-bwjs",
+
+$ServerName = "d-bwjd","d-bwjq","d-bwjp","d-bobjs","d-bobjd","d-bobjqa","d-bobjp","d-bws","d-bwd","d-bwq","d-bwp"
+DO
+{
+foreach ($Server in $ServerName) {
+
+		if (test-Connection -ComputerName $Server -Count 2 -Quiet ) { 
+		
+			write-Host "$Server is alive." -ForegroundColor Green
+		
+					} else
+					
+						{ Write-Warning "$Server may not be alive."
+			
+					}	
+		
+}
+Start-Sleep -s 5
+} While (1)
